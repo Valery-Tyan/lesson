679fe7117be710c8dbc8e3d9c939fe81a7442c0f
valery <tyan97@icloud.com>
2021-07-25 20:03:24 +0300

    1 ver

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const answer = [];
+answer[0] = prompt ("ширина", "");
+answer[1] = prompt ("длина", "");
+answer[2] = prompt ("высота", "");
+alert(`объем= ${answer[0] * answer[1] * answer[2]}`);
